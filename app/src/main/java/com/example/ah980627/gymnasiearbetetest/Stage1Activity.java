98331876e59920cbb3b74e811d9a986ad83df2fa
AronHising <aron.hising@live.com>
2016-11-09 14:15:51 +0100

    new commit

--- app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
+++ app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
@@ -1,16 +1,13 @@
 package com.example.ah980627.gymnasiearbetetest;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Stage1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_stage1);
     }
 }
