f647677300bd63abc2b7a18ff95904d34e9f7911
AronHising <aron.hising@live.com>
2016-11-16 14:11:14 +0100

    array with random number bet

--- app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
+++ app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
@@ -3,11 +3,27 @@ package com.example.ah980627.gymnasiearbetetest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
+
 public class Stage1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage1);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        Collections.shuffle(Arrays.asList(arr));
     }
-}
+    Integer[] arr = new Integer[5];
+    String[] aHardwareName= {"Hårddisk", "Grafikkort", "Processor", "PSU", "SSD"};
+
+
+    }
+
+
+
