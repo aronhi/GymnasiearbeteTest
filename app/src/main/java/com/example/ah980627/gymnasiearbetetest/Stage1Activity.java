7f461ab89342835dec23c26e8c1999a9edcc82d9
AronHising <aron.hising@live.com>
2016-11-29 14:30:38 +0100

    Fixar random text till stage1activity

--- app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
+++ app/src/main/java/com/example/ah980627/gymnasiearbetetest/Stage1Activity.java
@@ -2,7 +2,6 @@ package com.example.ah980627.gymnasiearbetetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.Arrays;
